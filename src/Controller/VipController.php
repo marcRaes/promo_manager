6a7a59e9c159c0e95bf552ae63a7acc943b64dee
marcRaes <contact@marcraes.fr>
2025-04-30 10:14:02 +0200

    Mise en place du projet

---
+++ src/Controller/VipController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class VipController extends AbstractController
+{
+    #[Route('/vip', name: 'app_vip')]
+    #[IsGranted('VOTER_VIP')]
+    public function index(): Response
+    {
+        return $this->render('vip/index.html.twig', [
+            'controller_name' => 'VipController',
+        ]);
+    }
+}
