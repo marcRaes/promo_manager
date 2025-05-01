23accbfe869b7e3016e9ef2845d1b21a78926ba2
marcRaes <contact@marcraes.fr>
2025-05-01 18:52:05 +0200

    Création du CRUD sur les codes promo

--- src/Controller/CodeController.php
+++ src/Controller/CodeController.php
@@ -2,18 +2,74 @@
 
 namespace App\Controller;
 
+use App\Entity\Code;
+use App\Form\CodeForm;
 use App\Repository\CodeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/code')]
 final class CodeController extends AbstractController
 {
-    #[Route('/code', name: 'app_code')]
+    #[Route(name: 'app_code_index', methods: ['GET'])]
     public function index(CodeRepository $codeRepository): Response
     {
         return $this->render('code/index.html.twig', [
             'codes' => $codeRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_code_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $code = new Code();
+        $form = $this->createForm(CodeForm::class, $code);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $code->setUser($this->getUser());
+            $entityManager->persist($code);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_code_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('code/new.html.twig', [
+            'code' => $code,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_code_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Code $code, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CodeForm::class, $code);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $code->setUser($this->getUser());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_code_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('code/edit.html.twig', [
+            'code' => $code,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_code_delete', methods: ['POST'])]
+    public function delete(Request $request, Code $code, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$code->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($code);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_code_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/CodeForm.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Code;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CodeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('discount', NumberType::class, [
+                'label' => 'Réduction (%)',
+                'scale' => 2,
+            ])
+            ->add('domainName', TextType::class, [
+                'label' => 'Nom de domaine',
+            ])
+            ->add('validFrom', DateTimeType::class, [
+                'label' => 'Valide à partir de',
+            ])
+            ->add('validUntil', DateTimeType::class, [
+                'label' => 'Valide jusqu\'à',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Code::class,
+        ]);
+    }
+}
